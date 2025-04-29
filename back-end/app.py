66d7d98b24501d13da25322c32e30fb0315a5b42
TYL <tyl@example.com>
2025-04-29 19:41:56 +0800

    gcp run fix1941

--- back-end/app.py
+++ back-end/app.py
@@ -1,10 +1,6 @@
-# app.py
 from flask import Flask
 
 def create_app():
     app = Flask(__name__)
     # 在這裡添加路由和配置
     return app
-
-# 不要在這裡執行 app.run()
-# 如果要部署，Flask 應該由 Gunicorn 啟動，不需要在 app.py 中運行
